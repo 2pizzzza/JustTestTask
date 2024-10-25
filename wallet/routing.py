86181fd07d7b12865da3b05aa3258e40ba410a19
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-10-25 06:54:50 +0600

    minor: websocket

--- wallet/routing.py
+++ wallet/routing.py
@@ -3,5 +3,5 @@ from django.urls import path, re_path
 from .consumers import BitcoinPriceConsumer
 
 websocket_urlpatterns = [
-    path('ws/bitcoin_price/', BitcoinPriceConsumer.as_asgi()),
+    re_path(r'^ws/bitcoin_price/$', BitcoinPriceConsumer.as_asgi()),
 ]
